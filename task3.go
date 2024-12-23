09e2f73bebfce6008e4f5a2c4894bd77c610f77d
umi3awa <kereyk87@gmail.com>
2024-12-23 09:01:16 +0500

    Testing 8

---
+++ task3.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/umi3uwu/Assignment1/company"
+)
+
+func main() {
+	c := company.NewCompany()
+
+	c.AddEmployee(company.FullTimeEmployee{ID: 1, Name: "Alice", Salary: 5000})
+	c.AddEmployee(company.PartTimeEmployee{ID: 2, Name: "Bob", HourlyRate: 20, HoursWorked: 100})
+
+	c.ListEmployees()
+}
