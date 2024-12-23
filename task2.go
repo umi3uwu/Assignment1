3467fccbf51a7ca9281e7dad3c6bb03de5525175
umi3awa <kereyk87@gmail.com>
2024-12-23 08:54:57 +0500

    Testing 7

--- task2.go
+++ task2.go
@@ -5,14 +5,14 @@ import (
 )
 
 func main() {
-	shapes := []shapes.Shape{
+	shapesList := []shapes.Shape{
 		shapes.Rectangle{Length: 10, Width: 5},
 		shapes.Circle{Radius: 7},
 		shapes.Square{Length: 4},
 		shapes.Triangle{SideA: 3, SideB: 4, SideC: 5},
 	}
 
-	for _, shape := range shapes {
+	for _, shape := range shapesList {
 		shapes.PrintShapeDetails(shape)
 	}
 }
