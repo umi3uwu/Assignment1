3e82fac74ef079c090b4e0e424ad909ee0020cde
umi3awa <kereyk87@gmail.com>
2024-12-23 08:13:08 +0500

    Testing 2

--- main.go
+++ main.go
@@ -1,16 +1,62 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/umi3uwu/Assignment1/tree/master/Library"
+)
 
 func main() {
-	list := []int{1, 2, 3, 4}
-	fmt.Println("before", list)
-	handle(list)
-	fmt.Println("after", append(list, 5))
-}
-
-func handle(slice []int) {
-	newSlice := make([]int, len(slice))
-	newSlice = append(slice, 5)
-	fmt.Println("append", newSlice)
+	l := library.NewLibrary()
+	s := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Println("Enter command (Add, Borrow, Return, List, Exit):")
+		if !s.Scan() {
+			break
+		}
+		c := strings.TrimSpace(s.Text())
+		switch c {
+		case "Add":
+			fmt.Println("Enter book details in the format: ID,Title,Author")
+			if !s.Scan() {
+				break
+			}
+			i := strings.TrimSpace(s.Text())
+			d := strings.Split(i, ",")
+			if len(d) != 3 {
+				fmt.Println("Invalid input.")
+				continue
+			}
+			b := library.Book{
+				ID:     strings.TrimSpace(d[0]),
+				Title:  strings.TrimSpace(d[1]),
+				Author: strings.TrimSpace(d[2]),
+			}
+			l.AddBook(b)
+		case "Borrow":
+			fmt.Println("Enter the Book ID to borrow:")
+			if !s.Scan() {
+				break
+			}
+			id := strings.TrimSpace(s.Text())
+			l.BorrowBook(id)
+		case "Return":
+			fmt.Println("Enter the Book ID to return:")
+			if !s.Scan() {
+				break
+			}
+			id := strings.TrimSpace(s.Text())
+			l.ReturnBook(id)
+		case "List":
+			l.ListBooks()
+		case "Exit":
+			fmt.Println("Exiting program...")
+			return
+		default:
+			fmt.Println("Invalid command.")
+		}
+	}
 }
