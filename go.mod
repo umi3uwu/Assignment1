83489592c463bbd3a4231b662fd73518e6e31b2e
umi3awa <kereyk87@gmail.com>
2025-01-29 19:34:28 +0500

    Front HAndler

--- go.mod
+++ go.mod
@@ -1,1 +1,3 @@
 module github.com/umi3uwu/Assignment1
+
+go 1.23.4
--- task1/task1.go
+++ task1/task1.go
@@ -6,7 +6,7 @@ import (
 	"os"
 	"strings"
 
-	"github.com/umi3uwu/Assignment1/Library"
+	"github.com/umi3uwu/Assignment1/library"
 )
 
 func main() {
